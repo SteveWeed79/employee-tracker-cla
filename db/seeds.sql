ddf6a9eecc7b7857c9c2554d4d2d9be925dc4242
unknown <steveweed1979@gmail.com>
2022-08-30 23:47:22 -0500

    Worked on schema, seeding, server.js

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Management"),
+       ("Accounting");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1),
+       ("Salesperson", 40000, 1),
+       ("Lead Engineer", 200000, 2),
+       ("software Engineer", 100000, 2),
+       ("Engineering Manager", 250000, 3),
+       ("Accountant", 75000, 4 ),
+       ("Lead Accountant", 150000, 4);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Bob", "Smith", 1, 4),
+       ("Sally", "Sue", 4, 0),
+       ("Larry", "Jones", 2, 4),
+       ("Melissa", "Doura", 3, 0),
+       ("LaDee", "Bicknese", 1, 4),
+       ("Stanley", "Caterson", 2, 4),
+       ("Jason", "Worth", 1, 0);
