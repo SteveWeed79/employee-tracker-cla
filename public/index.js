596680a7bf9b7c0539912d9ff6a9a670e96466e0
unknown <steveweed1979@gmail.com>
2022-09-02 16:12:13 -0500

    Started work on server.js and inquirer

--- public/index.js
+++ public/index.js
@@ -1,1 +1,17 @@
 const inquirer = require('inquirer')
+
+function employee() {
+
+    inquirer
+        .prompt([
+
+            {
+                type: 'list',
+                name: 'role',
+                message: 'What would you like to do?',
+                choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add A Role', 'View Departments', 'Add Department']
+            },
+
+        ])
+
+};
