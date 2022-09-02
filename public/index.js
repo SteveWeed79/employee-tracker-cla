786ee305afce6e973dbaa6082999dbcda879e556
unknown <steveweed1979@gmail.com>
2022-09-02 16:31:51 -0500

    Worked in server.js on inquirer set up for roles and employee

--- public/index.js
+++ public/index.js
@@ -7,11 +7,36 @@ function employee() {
 
             {
                 type: 'list',
-                name: 'role',
+                name: 'tasks',
                 message: 'What would you like to do?',
                 choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add A Role', 'View Departments', 'Add Department']
             },
+            {
+                type: 'input',
+                name: 'first_name',
+                message: 'Please enter the employees first name.'
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'Please enter the employees last name.'
+            },
+            {
+                type: 'input',
+                name: 'addRoleTitle',
+                message: 'What is the name of the role.'
+            },
+            {
+                type: 'input',
+                name: 'addRoleSalary',
+                message: 'What is the salary for this role?',
+            },
+            {
+                type: 'list',
+                name: 'addRoleDept',
+                message: 'What department is this role in?',
+                list: []
+            },
 
         ])
-
 };
